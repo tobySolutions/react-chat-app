622084f7b858f11118419d25597fbfdaf412d5f1
Tobiloba Adedeji <96334363+tobySolutions@users.noreply.github.com>
2023-01-17 10:00:16 +0100

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -11,7 +11,7 @@ const Message = ({message}) => {
       <div className="chat-bubble__right">
         <p className="user-name">{message.name}</p>
         <p className="user-message">
-         {message.name}
+         {message.text}
         </p>
       </div>
     </div>
